020802e12541c7d6b4746bb879a243113b9c4b6f
spykelion <ndilionel6@gmail.com>
2022-03-16 12:31:11 +0100

    consume state in index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Chat from "./Chat";
+import Login from "./Login";
+import Sidebar from "./Sidebar";
+import { useStateValue } from './StateProvider';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [{ user }, ] = useStateValue();
+  console.log(user)
+  return user && (
+   <BrowserRouter>
+     <div className="app">
+      <div className="app__body">
+        <Sidebar />
+      <Routes>
+            <Route path="/rooms/:roomId" element={<Chat />} />
+            <Route path="*" element={<>NOt FOund</>} />
+      </Routes>
+      </div>
     </div>
-  );
+   </BrowserRouter>
+  ) || (<Login />)
 }
 
 export default App;
