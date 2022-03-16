3d9f350e088dc4484e27dda525f4b0cfd9c45c68
spykelion <ndilionel6@gmail.com>
2022-03-16 12:25:54 +0100

    create sidebarChat component

---
+++ src/SidebarChat.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Avatar, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Link } from 'react-router-dom';
+import { collection, addDoc, query, orderBy, onSnapshot } from "firebase/firestore";
+
+import db from './firebase';
+import './SidebarChat.css';
+
+function SidebarChat({ id, name, addNewChat }) {
+    const [seed, setSeed] = useState("");
+    const [messages, setMessages] = useState([]);
+    const roomCollectionRef = collection(db, "rooms");
+
+    useEffect(()=>{
+        const getMessagesDocs = async () => {
+            try {
+              const mq = query(collection(db, "rooms", id, "messages"))
+              onSnapshot(mq, (querySnapshot) => {
+                    setMessages(querySnapshot.docs.map(doc => doc.data()))
+                    console.log("FROM SIDEBAR - chat => /-trim/",querySnapshot.docs.map(doc =>
+                        ({
+                            id: doc.id,
+                            data: doc.data(),
+                        })))
+                });
+      
+            } catch (error) {
+            //   console.error(error);
+            }
+          };
+          getMessagesDocs()
+    },[])
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
+    const createChat = async() => {
+        const roomName = prompt("Please enter name for chat");
+       
+        if (roomName) {
+            await addDoc(roomCollectionRef, {name: roomName})
+        }
+    };
+
+    return !addNewChat ? (
+        <Link to={`/rooms/${id}`}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>{messages[0]?.body}</p>
+                </div>
+            </div>
+        </Link>
+    ) : (
+        <div onClick={createChat} className="sidebarChat">
+             <IconButton>
+            <AddIcon />
+          </IconButton>
+        </div>
+    )
+}
+
+export default SidebarChat;
